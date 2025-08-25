02e79fda137956483468a0d97f9b0bddf3078a0b
patricklafrance <pa.lafrance@gmail.com>
2025-08-24 21:45:36 -0400

    refactor(logging): remove unused type Segment from Logger export

--- packages/logging/src/index.ts
+++ packages/logging/src/index.ts
@@ -1,6 +1,6 @@
 export { BrowserConsoleLogger, BrowserConsoleLoggerScope } from "./BrowserConsoleLogger.ts";
 export { CompositeLogger, CompositeLoggerScope } from "./CompositeLogger.ts";
 export { createCompositeLogger } from "./createCompositeLogger.ts";
-export { LogLevel, type Logger, type LoggerOptions, type LoggerScope, type LoggerScopeEndOptions, type LoggerScopeOptions, type LogOptions, type RootLogger, type Segment } from "./Logger.ts";
+export { LogLevel, type Logger, type LoggerOptions, type LoggerScope, type LoggerScopeEndOptions, type LoggerScopeOptions, type LogOptions, type RootLogger } from "./Logger.ts";
 export { NoopLogger, NoopLoggerScope } from "./NoopLogger.ts";
 
