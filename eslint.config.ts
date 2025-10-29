2495ee74c588ee9d40b48aa3275a7bf906a2856c
patricklafrance <pa.lafrance@gmail.com>
2025-10-29 17:53:42 -0400

    refactor(eslint): remove 'templates' from global ignores in ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,6 @@ export default defineConfig([
     globalIgnores([
         "packages",
         "samples",
-        "templates",
         "docs"
     ]),
     defineMonorepoWorkspaceConfig(import.meta.dirname)
